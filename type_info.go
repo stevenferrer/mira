64b63ef02464fcd61e3802a1836c56e21d7f99da
Steven Ferrer <steven.r.ferrer@gmail.com>
2022-02-24 23:32:08 +0800

    Update TypeInfo struct
    - Use value receiver instead of pointer receiver

--- type_info.go
+++ type_info.go
@@ -11,44 +11,44 @@ type TypeInfo struct {
 }
 
 // NewTypeInfo instrospects v and returns a TypeInfo
-func NewTypeInfo(v interface{}) *TypeInfo {
-	return &TypeInfo{v: v, t: reflect.TypeOf(v)}
+func NewTypeInfo(v interface{}) TypeInfo {
+	return TypeInfo{v: v, t: reflect.TypeOf(v)}
 }
 
 // V returns the interface value
-func (ti *TypeInfo) V() interface{} {
+func (ti TypeInfo) V() interface{} {
 	return ti.v
 }
 
 // T returns the reflect.Type
-func (ti *TypeInfo) T() reflect.Type {
+func (ti TypeInfo) T() reflect.Type {
 	return ti.t
 }
 
-// Name returns the type's name within its package
-// for a defined type.
+// Name returns the name of the type within
+// its package for a defined type.
 //
 // This also works with arrays, slices and pointers.
-func (ti *TypeInfo) Name() string {
+func (ti TypeInfo) Name() string {
 	return name(ti.t)
 }
 
-// IsNillable returns true when the type is nillable
-// i.e. slices, maps and pointers
-func (ti *TypeInfo) IsNillable() bool {
+// IsNillable returns true if the type is
+// nillable (i.e. slices, maps and pointers)
+func (ti TypeInfo) IsNillable() bool {
 	return isNillable(ti.t)
 }
 
-// IsNumeric returns true when the type is numeric
-// i.e. int, uint or float.
-func (ti *TypeInfo) IsNumeric() bool {
+// IsNumeric returns true if the type is
+// numeric i.e. int, uint or float.
+func (ti TypeInfo) IsNumeric() bool {
 	return isNumeric(ti.t)
 }
 
 // PkgPath returns a defined type's package path.
 //
 // This also works with arrays, slices, maps and pointers.
-func (ti *TypeInfo) PkgPath() string {
+func (ti TypeInfo) PkgPath() string {
 	return pkgPath(ti.t)
 }
 
